9e0a9a884d1b243245f2575f5902accf59397a3d
Mahmoud97Hesham <mahmoud_hesham1997@yahoo.com>
2019-03-19 02:14:16 +0200

    Parsing punctuations and keywords done.

--- LexicalRulesParser.cpp
+++ LexicalRulesParser.cpp
@@ -4,3 +4,67 @@
 
 #include "LexicalRulesParser.h"
 
+
+vector<string> LexicalRulesParser::split(string stringToSplit,char delimiter){
+    vector<string> splitedString;
+    stringstream ss(stringToSplit); // Turn the string into a stream.
+    string tok;
+    while(getline(ss, tok, delimiter)) {
+        splitedString.push_back(tok);
+    }
+    return splitedString;
+}
+
+void LexicalRulesParser::handleKeywords(string keywords){
+    vector<string>splitedString = split(keywords,' ');
+    for(int i = 0;i < splitedString.size();i++){
+        if(splitedString[i] == "{" || splitedString[i] == "}"){
+            continue;
+        }
+        else if(splitedString[i].find('{') != string::npos){
+            splitedString[i].erase(remove(splitedString[i].begin(), splitedString[i].end(), '{'), splitedString[i].end());
+            Allkeywords.push_back(splitedString[i]);
+        }
+        else if(splitedString[i].find('}') != string::npos){
+            splitedString[i].erase(remove(splitedString[i].begin(), splitedString[i].end(), '}'), splitedString[i].end());
+            Allkeywords.push_back(splitedString[i]);
+        }
+        else{
+            Allkeywords.push_back(splitedString[i]);
+        }
+    }
+}
+
+void LexicalRulesParser::handlePunctuations(string punctuations){
+    vector<string> splitedString = split(punctuations,' ');
+    for(int i = 0;i < splitedString.size();i++){
+        if(splitedString[i] == "[" || splitedString[i] == "]"){
+            continue;
+        }
+        else if(splitedString[i].find('[') != string::npos){
+            splitedString[i].erase(remove(splitedString[i].begin(), splitedString[i].end(), '['), splitedString[i].end());
+            Allpunctuations.push_back(splitedString[i]);
+        }
+        else if(splitedString[i].find(']') != string::npos){
+            splitedString[i].erase(remove(splitedString[i].begin(), splitedString[i].end(), ']'), splitedString[i].end());
+            Allpunctuations.push_back(splitedString[i]);
+        }
+        else{
+            Allpunctuations.push_back(splitedString[i]);
+        }
+    }
+}
+
+void LexicalRulesParser::readLexicalRules(string fileName){
+    ifstream file(fileName);
+    string line;
+    while(getline(file,line)){
+        if(line.at(0) == '{'){
+            handleKeywords(line);
+        }
+        else if(line.at(0) == '['){
+            handlePunctuations(line);
+        }
+    }
+}
+
--- LexicalRulesParser.h
+++ LexicalRulesParser.h
@@ -5,4 +5,27 @@
 #ifndef LEXICALANALYZER_LEXICALRULESPARSER_H
 #define LEXICALANALYZER_LEXICALRULESPARSER_H
 
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+
+class LexicalRulesParser {
+    vector<string> Allkeywords;
+    vector<string> Allpunctuations;
+
+public:
+    void readLexicalRules(string fileName);
+
+    void handleKeywords(string keywords);
+
+    vector<string> split(string stringToSplit, char delimiter);
+
+    void handlePunctuations(string punctuations);
+};
+
 #endif //LEXICALANALYZER_LEXICALRULESPARSER_H
