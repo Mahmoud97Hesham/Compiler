b6f646f0a221128031ab3a10c1868a651768f9ca
EshraqIbrahim <eshraq.ibrahim@gmail.com>
2019-04-09 23:37:32 +0200

    Kolo tmaam 5laas

--- MinimizeDFA.cpp
+++ MinimizeDFA.cpp
@@ -1,5 +1,4 @@
 #include <utility>
-
 #include "MinimizeDFA.h"
 #include <map>
 #include <iostream>
@@ -25,33 +24,22 @@ vector<DFAState> MinimizeDFA::minimize() {
             vector<int> currentStateTransitions;
             for (int j = 0; j < input.size(); j++) {
                 currentStateTransitions.push_back(currentDFA.getTransition(input.at(j)));
-
             }
             if (allInputsTransitions.find(currentStateTransitions) == allInputsTransitions.end() && (currentDFA.isAcceptable())) {
                 allInputsTransitions[currentStateTransitions] = currentDFA.getID();
-                cout << " ADD TO ACCEPTED MAP " << currentDFA.getID();
-
             }else if(allInputsTransitionsNotaccepted.find(currentStateTransitions) == allInputsTransitionsNotaccepted.end() &&(!currentDFA.isAcceptable())){
                 allInputsTransitionsNotaccepted[currentStateTransitions] = currentDFA.getID();
-                cout << " ADD TO NOT ACCEPTED MAP " << currentDFA.getID();
             }else {
                 bool isFirstStateAcceptable = currentDFA.isAcceptable();
-//                DFAState alternativeState = completeDFAMap.at(allInputsTransitions[currentStateTransitions]);
-//                bool isSecondStateAcceptable = alternativeState.isAcceptable();
-                cout << " ID " << currentDFA.getID() << " FIRST STATE BOOL " << isFirstStateAcceptable << endl;
-//                cout << " ID " << alternativeState.getID()  << " SECOND STATE BOOL " << isSecondStateAcceptable << endl;
                 if (isFirstStateAcceptable) {
-
                     DFAState alternativeState = completeDFAMap.at(allInputsTransitions[currentStateTransitions]);
                     if(alternativeState.getToken() == currentDFA.getToken()){
                         duplicatesMap[currentDFA.getID()] = allInputsTransitions[currentStateTransitions];
                         checkDuplicates = true;
                     }
-                    //cout << " ID " << allInputsTransitions[currentStateTransitions] << " ACCEPTED" << endl;
                 }else{
                     duplicatesMap[currentDFA.getID()] = allInputsTransitionsNotaccepted[currentStateTransitions];
                     checkDuplicates = true;
-                    //cout << " ID " << allInputsTransitions[currentStateTransitions] << " NOT ACCEPTED" << endl;
                 }
 
             }
