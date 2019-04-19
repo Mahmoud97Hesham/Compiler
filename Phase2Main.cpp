d628a0d6702a78b4486e71572c1a31d234f97f24
Morcos Sameh <morcossameh@gmail.com>
2019-04-20 01:33:10 +0200

    ParserGenarator created

--- Phase2Main.cpp
+++ Phase2Main.cpp
@@ -1,27 +1,57 @@
 #include <iostream>
-#include "ProductionRule.h"
+#include "ParserGenerator.h"
 using namespace std;
 
 int main() {
   vector<ProductionRule> prVec;
 
-  string s = "METHOD_BODY";
-  Symbol s1 = Symbol(s);
-  ProductionRule pr1 = ProductionRule(s1);
-  Symbol s2 = Symbol("STATEMENT_LIST");
-  pr1.addSymbol(s2, true);
-
+  Symbol S = Symbol("S");
+  Symbol A = Symbol("A");
+  Symbol B = Symbol("B");
+  Symbol C = Symbol("C");
+  Symbol D = Symbol("D");
+  Symbol E = Symbol("E");
+  Symbol a = Symbol("a");
+  Symbol b = Symbol("b");
+  Symbol c = Symbol("c");
+  Symbol d = Symbol("d");
+  Symbol e = Symbol("e");
+  Symbol epsilon = Symbol("epsilon");
+  
+  ProductionRule pr1 = ProductionRule(S);
+  pr1.addSymbol(A, true);
+  pr1.addSymbol(B, false);
+  pr1.addSymbol(C, false);
+  pr1.addSymbol(D, false);
+  pr1.addSymbol(E, false);
   prVec.push_back(pr1);
 
+  cout << A.isEqual("a") << endl;   // should print true
   
-  cout << s2.isEqual("STATEMENT_LIST") << endl;   // should print true
-  
-  ProductionRule pr2 = ProductionRule(s2);
-  Symbol s3 = Symbol("STATEMENT");
-  pr2.addSymbol(s3, true);
-  pr2.addSymbol(s2, true);
-  pr2.addSymbol(s3, false);
-
+  ProductionRule pr2 = ProductionRule(A);
+  pr2.addSymbol(a, true);
+  pr2.addSymbol(epsilon, true);
   prVec.push_back(pr2);
+  
+  ProductionRule pr3 = ProductionRule(B);
+  pr3.addSymbol(b, true);
+  pr3.addSymbol(epsilon, true);
+  prVec.push_back(pr3);
+  
+  ProductionRule pr4 = ProductionRule(C);
+  pr4.addSymbol(c, true);
+  prVec.push_back(pr4);
+  
+  ProductionRule pr5 = ProductionRule(D);
+  pr5.addSymbol(d, true);
+  pr5.addSymbol(epsilon, true);
+  prVec.push_back(pr5);
+  
+  ProductionRule pr6 = ProductionRule(E);
+  pr6.addSymbol(e, true);
+  pr6.addSymbol(epsilon, true);
+  prVec.push_back(pr6);
+  
+  ParserGenerator pg = ParserGenerator(prVec);
   return 0;
 }
